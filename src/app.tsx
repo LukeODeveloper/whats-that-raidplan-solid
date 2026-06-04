48869ffc68b346bace404066421bf22158a44808
LukeODeveloper <luke.oxer.developer@gmail.com>
2026-06-04 13:00:32 +0100

    added files

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,5 @@
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { Suspense } from "solid-js";
-import Nav from "~/components/Nav";
 import Layout  from "~/layout";
 import "./app.css";
 
